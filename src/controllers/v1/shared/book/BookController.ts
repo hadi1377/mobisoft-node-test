a5c60d18cd60234d188caaf48b5b0ec5f0e548e2
Hadi Ahmadzadeh <hadi.wf77@gmail.com>
2021-02-04 01:50:35 +0330

    + Initial book router and start controller

---
+++ src/controllers/v1/shared/book/BookController.ts
@@ -0,0 +1,3 @@
+class BookController {}
+
+export default BookController;
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import setUser from "@middlewares/setUser";
 import authRouter from "./auth";
+import bookRouter from "./book";
 
 const router = Router();
 
 router.use("/", setUser);
 router.use("/auth", authRouter);
+router.use(bookRouter);
 
 export default router;
