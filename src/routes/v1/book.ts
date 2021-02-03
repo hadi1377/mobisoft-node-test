7c199ae73156ac3b1d82edb469ecae8b60f20244
Hadi Ahmadzadeh <hadi.wf77@gmail.com>
2021-02-04 03:22:51 +0330

    + Upload book image

--- src/routes/v1/book.ts
+++ src/routes/v1/book.ts
@@ -1,10 +1,19 @@
-import BookController from "@controllers/v1/shared/book/BookController";
 import { Router } from "express";
 
+import Multer from "@config/Multer";
+import BookController from "@controllers/v1/shared/book/BookController";
+import { isAuth } from "@middlewares/AuthMiddlewares";
+
 const bookController = new BookController();
+const uploader = new Multer("books");
 
 const router = Router();
 
-router.post("/book", bookController.createBook);
+router.post(
+  "/book",
+  isAuth,
+  uploader.createDisk().single("image"),
+  bookController.createBook
+);
 
 export default router;
