a154fbd2703711a079f29c4086f135a91f36f2dc
Hadi Ahmadzadeh <hadi.wf77@gmail.com>
2021-02-04 00:47:49 +0330

    Update db connection and remove mysql2

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME || "root",
   process.env.DB_PASSWORD || "",
   {
-      dialect: 'mysql',
+      dialect: <any>process.env.DB_DRIVER || "postgres",
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT) || 3306,
   }
